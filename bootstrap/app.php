520ac0ce3c5df27870778d97379abc69325ef556
Marisa Clardy <marisa@clardy.eu>
2019-11-23 23:33:20 -0600

    Implement console command functionality.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,12 +10,22 @@ $app->singleton(
 );
 
 $app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
     Elephant\Contracts\Mail\Kernel::class,
     App\Mail\Kernel::class
 );
 
 $app->singleton(
     Elephant\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
 );
 
