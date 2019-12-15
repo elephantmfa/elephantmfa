8e481e33a08d4357f38cf65fcf7a4ce4c6ae42ee
Marisa Clardy <marisa@clardy.eu>
2019-12-14 21:42:40 -0600

    Add clamav settings.

--- config/scanners.php
+++ config/scanners.php
@@ -13,12 +13,14 @@ return [
     |
     */
     'spamassassin' => [
-        // Host or path to socket where spamd is running.
+        // Host or path to socket where SpamD is running.
         //   Use ipv4:// for an IPv4 address. Must be IP:port format.
         //   Use ipv6:// for an IPv6 address. Must be [IP]:port format.
         //   Use unix:// for a Unix socket.
         'socket' => env('SPAMASSASSIN_PORT', 'ipv4://127.0.0.1:783'),
-        // The total bytes sent to SpamAssassin
+        // The total bytes sent to SpamAssassin. Set to 0 to send full email.
+        //   Sending full email will cause SpamAssassin scanning to take much
+        //   longer.
         'max_size' => 128 * 1000, // 128 Kb
         // The timeout until reading from SpamAssassin is given up.
         'timeout' => 60, // seconds
@@ -31,5 +33,35 @@ return [
                 '-u ubuntu'
             ],
         ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ClamAV
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for utilizing ClamAV as a scanner. ClamAV must
+    | by running as clamd, and Elephant will communicate with it.
+    |
+    */
+    'clamav' => [
+        // Host or path to socket where ClamAV is running.
+        //   Use ipv4:// for an IPv4 address. Must be IP:port format.
+        //   Use ipv6:// for an IPv6 address. Must be [IP]:port format.
+        //   Use unix:// for a Unix socket.
+        'socket' => env('CLAMAV_PORT', 'ipv4://127.0.0.1:3310'),
+        // Only send files that are smaller than `max_size` in bytes.
+        //   Note: Unless an Attachment's size is specified, the size will
+        //   be estimated. Thus, larger files may be sent, so smaller is faster.
+        'max_size' => 64 * 1000, // 64 Kb
+        // Whether or not to send the full email to ClamAV.
+        //   This is useful for scanning for phishing signatures from ClamAV.
+        'send_email' => [
+            'enabled' => true,
+            // The total bytes sent to ClamAV. Set to 0 to send full email.
+            //   Sending full email will cause ClamAV scanning to take much
+            //   longer.
+            'max_size' => 128 * 1000, // 128 Kb
+        ]
     ],
 ];
